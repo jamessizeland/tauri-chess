fbdbf9b1f00218b0b7c0b12658b5b2c5aec6434d
James Sizeland <jamessizeland@gmail.com>
2022-06-08 23:26:03 +0100

    add color picker to generate frontend data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
+import { SketchPicker, Color } from 'react-color';
 
 function App() {
-  return <div></div>;
+  const [color, setColor] = useState<Color>('#1fa9f4');
+
+  return (
+    <div>
+      <p>Hello Tauri</p>
+      <SketchPicker
+        color={color}
+        onChange={(color, event) => {
+          setColor(color.hex);
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
