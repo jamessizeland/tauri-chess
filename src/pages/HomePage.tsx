2ed0a13ec3dc8399c6a2231e1f9aa3d223bd4c83
James Sizeland <jamessizeland@gmail.com>
2022-06-10 20:28:05 +0100

    add start new game logic on page load

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
-import Chessboard from 'chessboardjsx';
+import React, { useState, useEffect } from 'react';
+import Chessboard, { Position } from 'chessboardjsx';
+import { notify } from 'services/notifications';
+
+// https://chessboardjsx.com/
 
 const HomePage = (): JSX.Element => {
-  const [position, setPosition] = useState<string>('start');
+  const [position, setPosition] = useState<Position | 'start'>('start');
+
+  useEffect(() => {
+    // set a new board up when we go to this page
+    notify('starting new game');
+    setPosition('start');
+  }, []);
+
   return (
     <div className="animate-backInRight animate-fast">
-      <h1 className="text-3xl font-bold underline text-center">Home Page</h1>
-      <div className="m-10 border-black">
-        <Chessboard width={400} position={position} />
+      <h1 className="text-3xl font-bold underline text-center">Game</h1>
+      <div className="flex justify-around items-center flex-wrap pt-5">
+        <Chessboard
+          id="testBoard"
+          width={400}
+          position={position}
+          sparePieces
+          // onDrop={}
+          // onMouseOverSquare={}
+          // onMouseOutSquare={}
+          boardStyle={{
+            borderRadius: '5px',
+            boxShadow: `0 5px 15px rgba(0,0,0,0.5)`,
+          }}
+          // squareStyles={}
+          // dropSquareStyle={}
+          // onDragOverSquare={}
+          // onSquareClick={}
+          // onSquareRightClick={}
+        />
       </div>
     </div>
   );
