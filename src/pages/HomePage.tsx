dae80d2a0fcb828422f9df8e4af2a186a292741c
James Sizeland <jamessizeland@gmail.com>
2022-06-10 19:55:17 +0100

    add chessboardjsx and render empty board

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Chessboard from 'chessboardjsx';
 
 const HomePage = () => (
   <div className="animate-backInRight animate-fast">
     <h1 className="text-3xl font-bold underline text-center">Home Page</h1>
-    <br />
+    <div className="m-10 border-black">
+      <Chessboard width={400} />
+    </div>
   </div>
 );
 
