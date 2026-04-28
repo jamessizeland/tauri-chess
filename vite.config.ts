8bcb54b3b8378d36096ef929d6a31d37c2fbafa8
James Sizeland <65866907+jamessizeland@users.noreply.github.com>
2026-04-28 18:41:25 +0100

    Revert "Update repo" (#27)
    Reverts jamessizeland/tauri-chess#26

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
@@ -7,10 +8,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), svgr(), tailwindcss()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [react(), tsconfigPaths(), svgr(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
